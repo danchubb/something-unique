7e6884a70b23c652e919073e021e815e3566cc94
danchubb <danielchubb@gmail.com>
2022-03-15 13:33:20 +0000

    flake8

---
+++ tests/test_unit/test_maths.py
@@ -0,0 +1,9 @@
+from something_unique.maths import add_numbers
+
+def test_add_numbers():
+        assert add_numbers(1, 2) == 3
+        assert add_numbers(10, 100) == 110
+
+def test_add_numbers_2():
+            assert add_numbers(1, 3) == 3
+            assert add_numbers(10, 2100) == 110
